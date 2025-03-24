21bc44c6f45dc2e9befd5b06917a488a62083712
codeajj <paavobro@gmail.com>
2025-03-24 12:23:34 +0200

    obama

--- Python/Moduuli 9/teht2.py
+++ Python/Moduuli 9/teht2.py
@@ -1,5 +1,3 @@
-import time
-
 class Auto:
     def __init__(self, rekisteri, huippunopeus):
         self.rekisteri = rekisteri
@@ -18,15 +16,15 @@ Auto1 = Auto("ABC-123", "200km/h")
 kmh = 30
 Auto1.kiihdytä(kmh)
 print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
-time.sleep(1)
+
 kmh = kmh + 70
 Auto1.kiihdytä(kmh)
 print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
-time.sleep(1)
+
 kmh = kmh + 50
 Auto1.kiihdytä(kmh)
 print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
-time.sleep(1)
+
 kmh = - 200
 Auto1.kiihdytä(kmh)
 print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
--- Python/Moduuli 9/teht3.py
+++ Python/Moduuli 9/teht3.py
@@ -1,8 +1,8 @@
 class Auto:
-    def __init__(self, rekisteri, huippunopeus, nopeus, kuljettumatka):
+    def __init__(self, rekisteri, huippunopeus):
         self.rekisteri = rekisteri
         self.huippunopeus = huippunopeus
-        self.kuljettumatka = kuljettumatka
+        self.kuljettumatka = 0
         self.nopeus = 0
 
     def kiihdytä(self, kmh):
@@ -15,28 +15,28 @@ class Auto:
         self.kuljettumatka = self.kuljettumatka + (self.nopeus * hours)
         return
 
-Auto1 = Auto("ABC-123", "220km/h", "0", "0")
+Auto1 = Auto("ABC-123", "200km/h")
 
 kmh = 30
 Auto1.kiihdytä(kmh)
 Auto1.kuljettu(1.5)
 print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka:.0f}")
 
-kmh = kmh + 100
+kmh = kmh + 70
 Auto1.kiihdytä(kmh)
 Auto1.kuljettu(1)
-print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
+print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka:.0f}")
 
-kmh = kmh + 40
+kmh = kmh + 50
 Auto1.kiihdytä(kmh)
 Auto1.kuljettu(1)
-print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
+print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka:.0f}")
 
 kmh = - 200
 Auto1.kiihdytä(kmh)
-print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
+print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka:.0f}")
 
 kmh = + 90
 Auto1.kiihdytä(kmh)
 Auto1.kuljettu(1)
-print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka}")
+print(f"Auton rekisteri: {Auto1.rekisteri}, huippunopeus {Auto1.huippunopeus}, nopeus {Auto1.nopeus}km/h ja kuljettu matka {Auto1.kuljettumatka:.0f}")
