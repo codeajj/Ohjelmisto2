273c4302dd57aa7e0b28381c59eb6a38a86d5af5
codeajj <paavobro@gmail.com>
2025-04-09 14:25:45 +0300

    obama

---
+++ JavaScript/Moduuli2/Tehtävä 3/teht3.js
@@ -0,0 +1,9 @@
+koirat = []
+for (let i = 6; i > 0; i--) {
+  nimi = prompt("Anna minulle " +i+ " koiran nimet")
+  koirat.push("<ul>" + "Koira: "+nimi+ "</ul>")
+}
+koirat.sort((a,b) => a-b);
+koirat.reverse();
+
+document.querySelector('#target').innerHTML = koirat
