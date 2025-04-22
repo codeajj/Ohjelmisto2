d0e22860f479c3df788a57ce795fd7096e5e3bde
codeajj <paavobro@gmail.com>
2025-04-22 16:09:59 +0300

    obama

--- JavaScript/Moduuli 3/Tehtävä 2/2.js
+++ JavaScript/Moduuli 3/Tehtävä 2/2.js
@@ -1,12 +1,10 @@
-const ulElement = document.querySelector('#target');
+const target = document.getElementById('target');
 
-const li = document.createElement('ul');
+const ul = document.createElement('ul');
 
-li.innerHTML = `<li>First item</li>
-                <li>Second item</li>
-                <li>Third item</li>`
+ul.innerHTML = `
+<li>First item</li>
+<li class="my-item">Second item</li>
+<li>Third item</li>`
 
-document.getElementById("target").appendChild(li);
-
-ulElement.classList.add('my-item')
-ulElement.innerHTML = li.innerHTML;
+target.appendChild(ul);
